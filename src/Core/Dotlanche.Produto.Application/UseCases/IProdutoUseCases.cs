9b1ac25f02853fff363098de6eb177c18158a488
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-11-12 20:55:55 -0300

    add Produto use cases

---
+++ src/Core/Dotlanche.Produto.Application/UseCases/IProdutoUseCases.cs
@@ -0,0 +1,14 @@
+﻿using DotLanche.Produto.Domain.Entities;
+
+namespace Dotlanche.Produto.Application.UseCases;
+
+public interface IProdutoUseCases
+{
+    Task Add(RegistroProduto produto);
+    Task<RegistroProduto> Edit(RegistroProduto produto);
+    Task<RegistroProduto> Delete(int idProduto);
+    Task<IEnumerable<RegistroProduto>> GetByCategoria(int idCategoria);
+    Task<IEnumerable<RegistroProduto>> GetOrderProducts(IEnumerable<Guid> orderList);
+    Task<RegistroProduto?> GetById(Guid id);
+    Task<RegistroProduto?> GetByName(string produtoName);
+}
---
+++ src/Core/Dotlanche.Produto.Application/UseCases/ProdutoUseCases.cs
@@ -0,0 +1,21 @@
+﻿using Dotlanche.Produto.Application.UseCases;
+using DotLanche.Produto.Application.Ports.Repositories;
+using DotLanche.Produto.Domain.Entities;
+
+namespace DotLanches.Application.Services;
+public class ProdutoUseCases:IProdutoUseCases
+{
+    private readonly IProdutoRepository _repository;
+    public ProdutoUseCases(IProdutoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Add(RegistroProduto produto) => await _repository.Add(produto);
+    public async Task<RegistroProduto> Edit(RegistroProduto produto) => await _repository.Edit(produto);
+    public async Task<RegistroProduto> Delete(int idProduto) => await _repository.Delete(idProduto);
+    public async Task<IEnumerable<RegistroProduto>> GetByCategoria(int idCategoria) => await _repository.GetByCategoria(idCategoria);
+    public async Task<IEnumerable<RegistroProduto>> GetOrderProducts(IEnumerable<Guid> orderList) => await _repository.GetOrderProducts(orderList);
+    public async Task<RegistroProduto?> GetById(Guid id) => await _repository.GetById(id);
+    public async Task<RegistroProduto?> GetByName(string produtoName) => await _repository.GetByName(produtoName);
+}
