31325fadc532bfcbbc6450e61a3863a5d4b3f7db
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-11-12 20:54:15 -0300

    add Produto entity to project

---
+++ src/Core/Dotlanche.Produto.Domain/Exceptions/DomainValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace DotLanche.Produto.Domain.Exceptions;
+
+public class DomainValidationException : Exception
+{
+    private const string MessageTemplate = "invalid value for {0}!";
+
+    public DomainValidationException(string propertyName) : base(string.Format(MessageTemplate, propertyName))
+    {
+    }
+}
