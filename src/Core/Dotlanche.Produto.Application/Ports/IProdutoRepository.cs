8429e263efb36285ccbece43b9cc5815c42822e9
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-11-12 20:55:23 -0300

    add repository interface port

---
+++ src/Core/Dotlanche.Produto.Application/Ports/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DotLanche.Produto.Domain.Entities;
+
+namespace DotLanche.Produto.Application.Ports.Repositories;
+
+public interface IProdutoRepository
+{
+    public Task<int> Add(RegistroProduto produto);
+    public Task<RegistroProduto> Edit(RegistroProduto produto);
+    public Task<RegistroProduto> Delete(int idProduto);
+    public Task<IEnumerable<RegistroProduto>> GetByCategoria(int idCategoria);
+    public Task<IEnumerable<RegistroProduto>> GetOrderProducts(IEnumerable<Guid> orderList);
+    public Task<RegistroProduto?> GetById(Guid idProduto);
+    public Task<RegistroProduto?> GetByName(string name);
+}
