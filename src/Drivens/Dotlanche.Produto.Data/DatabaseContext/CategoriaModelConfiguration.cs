82c90eeea390efbd441b34a003cb8253d42daa76
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-11-12 22:09:04 -0300

    data adapter implementation

---
+++ src/Drivens/Dotlanche.Produto.Data/DatabaseContext/CategoriaModelConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using DotLanche.Produto.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dotlanche.Produto.Data.DatabaseContext;
+
+internal class CategoriaModelConfiguration : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).IsRequired();
+    }
+}
---
+++ src/Drivens/Dotlanche.Produto.Data/Exceptions/CategoriaNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace Dotlanche.Produto.Data.Exceptions;
+
+internal class CategoriaNotFoundException : Exception
+{
+    public CategoriaNotFoundException() : base("Categoria not found!") { }
+}
