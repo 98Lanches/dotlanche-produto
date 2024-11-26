06537b625883b6ee7b7f152064dfa1057bcd5287
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-11-25 21:20:06 -0300

    refact: add key annotation to category

--- src/Core/Dotlanche.Produto.Domain/Entities/Categoria.cs
+++ src/Core/Dotlanche.Produto.Domain/Entities/Categoria.cs
@@ -1,9 +1,12 @@
 ﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace DotLanche.Produto.Domain.Entities;
 
 public class Categoria
 {
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; }
 }
